27dd7667730005ea5a1294e5ee1aef3bf2781a38
Angelo-CE <fabianceciliano@estudiantec.cr>
2023-11-11 13:10:17 -0600

    Pruebas unitarais 2

--- pruebaunitaria.py
+++ pruebaunitaria.py
@@ -11,25 +11,44 @@ class TestCocinero(unittest.TestCase):
         self.assertEqual(mc.listaCanciones, ["Canción 1", "Canción 2", "Canción 3"])
         self.assertEqual(mc.listaBailabilidad, [0.8, 0.9, 0.7])
 
+        # prueba 2
+        mc2 = MiClase(30, 210, 10, ["I'm Still standing Elton John", "Sirius Alan Parson", "Take on me a-ha"], [0.4, 0.6, 0.5])
+        self.assertEqual(mc2.Valencia, 30)
+        self.assertEqual(mc2.Tempo, 210)
+        self.assertEqual(mc2.Tonos, 10)
+        self.assertEqual(mc2.listaCanciones, ["I'm Still standing Elton John", "Sirius Alan Parson", "Take on me a-ha"])
+        self.assertEqual(mc2.listaBailabilidad, [0.4, 0.6, 0.5])
+
     def test_ObtieneValencia(self):
         # prueba 1
         mc = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
         self.assertEqual(mc.ObtieneValencia(1234567), 4)
-
+        # prueba 2
+        mc2 = MiClase(30, 210, 10, ["I'm Still standing Elton John", "Sirius Alan Parson", "Take on me a-ha"], [0.4, 0.6, 0.5])
+        self.assertEqual(mc2.ObtieneValencia(34567), 3)
     def test_DivisibleTempo(self):
         # prueba 1
         mc = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
         self.assertEqual(mc.DivisibleTempo(10), [1, 2, 5, 10])
+        # prueba 2
+        mc2 = MiClase(30, 210, 10, ["I'm Still standing Elton John", "Sirius Alan Parson", "Take on me a-ha"], [0.8, 0.9, 0.7])
+        self.assertEqual(mc2.DivisibleTempo(20), [1, 2, 4, 5, 10, 20])
 
     def test_ObtieneMasBailable(self):
         # prueba 1
         mc = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
         self.assertEqual(mc.ObtieneMasBailable([0.8, 0.9, 0.7]), 0.9)
+        # prueba 2
+        mc2 = MiClase(30, 210, 10, ["I'm Still standing Elton John", "Sirius Alan Parson", "Take on me a-ha"], [0.8, 0.9, 0.7])
+        self.assertEqual(mc2.ObtieneMasBailable([0.9, 0.5, 0.4]), 0.9)
 
     def test_VerificaListaCanciones(self):
         # prueba 1
         mc = MiClase(5, 120, 12, ["Canción 1", "Canción 2", "Canción 3"], [0.8, 0.9, 0.7])
         self.assertTrue(mc.VerificaListaCanciones(["Canción 1", "Canción 2", "Canción 3"]))
+        # prueba 2
+        mc2 = MiClase(30, 210, 10, ["I'm Still standing Elton John", "Sirius Alan Parson", "Take on me a-ha"], [0.8, 0.9, 0.7])
+        self.assertTrue(mc2.VerificaListaCanciones(["I'm Still standing Elton John", "Sirius Alan Parson", "Take on me a-ha"]))
 
 if __name__ == '__main__':
     unittest.main()
